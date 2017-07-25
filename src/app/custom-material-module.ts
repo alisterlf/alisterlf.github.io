abe8d57ae387a913d5ca679c50ba7bf456ac6e25
Álister Lopes Ferreira <alisterlf@gmail.com>
2017-07-25 16:52:33 -0300

    pushing dist folder

--- src/app/custom-material-module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {MdListModule, MdCheckboxModule, MdToolbarModule} from '@angular/material';
-
-@NgModule({
-  imports: [MdListModule, MdCheckboxModule, MdToolbarModule],
-  exports: [MdListModule, MdCheckboxModule, MdToolbarModule],
-})
-export class CustomMaterialModule { }
